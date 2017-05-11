2771c8f3535fa6d049976214222c90107cadb1a6
GaurwaithMelui <strategy155@gmail.com>
2017-05-11 15:02:47 +0300

    created bot; added functions hello, count_sym. tested code.

---
+++ src/bot_handler.py
@@ -0,0 +1,18 @@
+import telebot
+from src.definitions import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def welcome_sender(message):
+    bot.send_message(message.chat.id, "Здрасте, это бот, который считает длину вашего ( ͡° ͜ʖ ͡°) сообщения.")
+
+
+@bot.message_handler(func=lambda message: True)
+def length_sender(message):
+    bot.send_message(message.chat.id, "Ваше сообщение вместило {} символов".format(len(message.text)))
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
